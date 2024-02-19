2695b7bf36bc34f6cffe094f0a9240eb7a7002ee
MNnajir <121566044+MNnajir@users.noreply.github.com>
2024-02-19 20:16:54 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function color()
+{
+	var x = document.getElementById("result");
+	x.style.color="red";
+}
+function font_family()
+{
+	var x = document.getElementById("result");
+	x.style.fontFamily="calibri";
+}
